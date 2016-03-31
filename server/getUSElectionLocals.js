a79c861f591d6eb85b2ced4d51c6f507e162e19b
Sumeet Adur <sumeet.adur@ft.com>
2016-03-31 13:45:34 +0100

    Chart

--- server/getUSElectionLocals.js
+++ server/getUSElectionLocals.js
@@ -2,7 +2,7 @@ import cheerio from 'cheerio';
 import marked from 'marked';
 
 export default async function getUSElectionLocals() {
-	const url = `http://bertha.ig.ft.com/view/publish/gss/${process.env.US_ELECTION_SPREADSHEET_KEY}/options,+links`;
+	const url = `http://bertha.ig.ft.com/view/publish/gss/${process.env.US_ELECTION_SPREADSHEET_KEY}/options,links`;
 	const res = await fetch(url);
 	if (!res.ok) throw new Error(`Request failed with ${res.status}: ${url}`);
 
