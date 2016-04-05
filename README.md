f3f9f57507f8c92dce4454143a8a6342d63945d2
Callum Locke <callum.locke@gmail.com>
2016-04-05 14:54:09 +0100

    switch build system

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# brexit-stream-content
+# stream-fragments
 
-Assembles and outputs the 'metacard' used in the [Brexit stream](ft.com/brexit).
+> This is currently halfway through being renamed from 'brexit-stream-content' to 'stream-fragments'. The Heroku app is still called ft-ig-brexit-stream-content.
 
-Warning: this has no caching, no efficiency – it builds every response from scratch, including loading content from remote URLs. It will break if put under heavy load. Must have a CDN or poller in front of it.
+Assembles and outputs the 'metacard' used in the [Brexit stream](http://ft.com/brexit).
 
+Warning: this serverside application has no caching, no efficiency – it builds every response from scratch, including loading content from remote URLs. It will break if put under heavy load. Must have a CDN or poller in front of it.
+
+<!--
 ## Routes
 
 - `/metacard/fragment.json` – outputs the metacard HTML but in a JSON object in the form `{"fragment": "..."}`. The stream app polls this via ft-poller and inlines it into the page.
@@ -16,20 +19,20 @@ Warning: this has no caching, no efficiency – it builds every response from sc
 	- [CDN-fronted URL for iframing](http://www.ft.com/ig/brexit-metacard-iframe.html) – this doesn't work if you're opted into Next, but doesn't need to as it's for embedding in old FT.com only.
 
 - `/metacard/preview.html` – just for development – the metacard, wrapped in a document that mimics the outer structure of the Next stream app, so you can see how the metacard looks with inherited styles etc.
+ -->
 
 ## Developing
 
 1. clone this repo
 2. `npm install`
-3. Use two terminal tabs:
-	- `npm run start:dev` (start server via nodemon)
-	- `npm run styles:dev` (compile Sass in watch mode)
-4. Open http://localhost:5000/
-5. Code
+3. `npm develop`
+4. code away
+
+Things should refresh in your browser automatically.
 
 ## Deploying
 
-1. Optional: make a PR, and Heroku will deploy you a live [review app](https://blog.heroku.com/archives/2015/9/3/heroku_flow_pipelines_review_apps_and_github_sync) so you can double-check the preview.html route. (Heroku will post a link on the PR automatically.)
+1. Optional: make a PR into master, and Heroku will deploy you a live [review app](https://blog.heroku.com/archives/2015/9/3/heroku_flow_pipelines_review_apps_and_github_sync) so you can check it works OK in production. (Heroku will post a link automatically shortly after you create the PR.)
 2. When you merge a PR into master (or just commit directly to master) Heroku will deploy to the prod app.
 
 You can check deployment status on the Heroku dashboard.
---
+++ src/server/getUSElectionLocals.js
@@ -0,0 +1,79 @@
+import cheerio from 'cheerio';
+import marked from 'marked';
+
+export default async function getUSElectionLocals() {
+	const url = `http://bertha.ig.ft.com/view/publish/gss/${process.env.US_ELECTION_SPREADSHEET_KEY}/options,results,links`;
+	const res = await fetch(url);
+	if (!res.ok) throw new Error(`Request failed with ${res.status}: ${url}`);
+
+	const sheets = await res.json();
+
+	const { options, results } = sheets;
+
+	const data = {};
+	for (const { name, value } of options) data[name] = value;
+
+
+	let resultsData = [];
+	for (const { label, party, value, superdelegates, total, droppedout } of results) resultsData.push({ label, party, value, superdelegates, total, droppedout });
+
+	// sort by total delegates descending
+	resultsData.sort( (a, b) => {
+		return b.total - a.total;
+	});
+
+	// only use the last name for each candidate
+	resultsData = resultsData.map( candidate => {
+		candidate.label = candidate.label.split(" ");
+		candidate.label = candidate.label[candidate.label.length-1];
+
+		return candidate;
+	});
+
+	// split democrats and republicans so we can get the top 2 of each
+	const democrats = resultsData.filter( candidate => {
+		return !candidate.droppedout && candidate.party === 'democrats'
+	});
+	const republicans = resultsData.filter( candidate => {
+		return !candidate.droppedout && candidate.party === 'republicans'
+	});
+
+	// variables used inside template
+	data.democratFirstCandidate = democrats[0].label;
+	data.democratFirstCandidatePledgedDelegates = democrats[0].value;
+	data.democratFirstCandidateSuperdelegates = democrats[0].superdelegates;
+	data.democratFirstCandidateDelegates = democrats[0].total;
+
+	data.democratSecondCandidate = democrats[1].label;
+	data.democratSecondCandidatePledgedDelegates = democrats[1].value;
+	data.democratSecondCandidateSuperdelegates = democrats[1].superdelegates;
+	data.democratSecondCandidateDelegates = democrats[1].total;
+
+	data.republicanFirstCandidate = republicans[0].label;
+	data.republicanFirstCandidateDelegates = republicans[0].total;
+
+	data.republicanSecondCandidate = republicans[1].label;
+	data.republicanSecondCandidateDelegates = republicans[1].total;
+
+	data.republicanThirdCandidate = republicans[2].label;
+	data.republicanThirdCandidateDelegates = republicans[2].total;
+
+	// process text from markdown to html, then insert data-trackable attributes into any links
+	data.text = (() => {
+		const $ = cheerio.load(marked(data.text));
+		$('a[href]').attr('data-trackable', 'link');
+		return $.html();
+	})();
+	if (data.secondaryText) {
+		data.secondaryText = (() => {
+			const $ = cheerio.load(marked(data.secondaryText));
+			$('a[href]').attr('data-trackable', 'link');
+			return $.html();
+		})();
+	}
+
+	data.isBrightcove = typeof data.video === 'number';
+	data.isYoutube = !data.isBrightcove;
+
+	return data;
+}
