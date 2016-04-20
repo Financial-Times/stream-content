8a8babb6c885fd4afe95ceb5fa12ef5dda8a53ef
Sumeet Adur <sumeet.adur@ft.com>
2016-04-20 16:44:29 +0100

    Robot Week content

--- src/server/server.js
+++ src/server/server.js
@@ -30,7 +30,9 @@ const router = new Router();
 // precompile template functions
 const views = path.resolve(__dirname, 'views');
 
-// const renderBrexitCard = jade.compileFile(path.join(views, 'brexit/card.jade'));
+const renderAIRoboticsPreview = jade.compileFile(path.join(views, 'ai-robotics/preview.jade'));
+const renderAIRoboticsIframe = jade.compileFile(path.join(views, 'ai-robotics/iframe.jade'));
+
 const renderBrexitSummary = jade.compileFile(path.join(views, 'brexit/summary-card.jade'));
 const renderBrexitGuide = jade.compileFile(path.join(views, 'brexit/guide.jade'));
 const renderBrexitIframe = jade.compileFile(path.join(views, 'brexit/iframe.jade'));
@@ -46,7 +48,13 @@ const renderUsElection2016Preview = jade.compileFile(
 	path.join(views, 'us-election-2016/preview.jade')
 );
 
+const renderAIRoboticsSummary = jade.compileFile(path.join(views, 'ai-robotics/summary-card.jade'));
+
 const elements = {
+	'ai-robotics-summary': async ctx => {
+		ctx.set('Content-Type', 'application/json');
+		ctx.body = JSON.stringify({ fragment: renderAIRoboticsSummary() });
+	},
 	'brexit-summary': async ctx => {
 		ctx.set('Content-Type', 'application/json');
 		ctx.body = JSON.stringify({ fragment: renderBrexitSummary(await getBrexitLocals()) });
@@ -77,12 +85,6 @@ router
 		}
 	})
 
-	// // fragment (for inlining in Next stream page)
-	// .get('/metacard/fragment.json', async ctx => {
-	// 	ctx.set('Content-Type', 'application/json');
-	// 	ctx.body = JSON.stringify({ fragment: renderBrexitCard(await getBrexitLocals()) });
-	// })
-
 	// topic summary fragment
 	.get('/metacard/fragment-topic-summary.json', async ctx => {
 		ctx.set('Content-Type', 'application/json');
@@ -93,6 +95,38 @@ router
 	.get('/metacard/fragment-topic-guide.json', async ctx => {
 		ctx.set('Content-Type', 'application/json');
 		ctx.body = JSON.stringify({ fragment: renderBrexitGuide(await getBrexitLocals()) });
+	})
+
+	// iframe (for using on the Falcon brexit page)
+	.get('/ai-robotics/iframe.html', async ctx => {
+		ctx.set('Cache-Control', 'max-age=500');
+
+		try {
+			ctx.body = renderAIRoboticsIframe();
+		}
+		catch (error) {
+			console.error('ERROR!', error ? error.stack : error);
+
+			ctx.status = 500;
+			ctx.body = '<script>frameElement.height=0;frameElement.style=\'display:none\'</script>';
+			return;
+		}
+	})
+
+	// iframe (for using on the Falcon brexit page)
+	.get('/brexit/iframe.html', async ctx => {
+		ctx.set('Cache-Control', 'max-age=500');
+
+		try {
+			ctx.body = renderBrexitIframe(await getBrexitLocals());
+		}
+		catch (error) {
+			console.error('ERROR!', error ? error.stack : error);
+
+			ctx.status = 500;
+			ctx.body = '<script>frameElement.height=0;frameElement.style=\'display:none\'</script>';
+			return;
+		}
 	})
 
 	// iframe (for using on the Falcon brexit page)
@@ -133,12 +167,22 @@ router
 	})
 
 	// preview (for dev only)
+	.get('/ai-robotics/preview.html', async ctx => {
+		ctx.body = renderAIRoboticsPreview();
+	})
+
+	// preview (for dev only)
+	.get('/brexit/preview.html', async ctx => {
+		ctx.body = renderBrexitPreview(await getBrexitLocals());
+	})
+
+	// preview (for dev only)
 	.get('/us-election-2016/preview.html', async ctx => {
 		ctx.body = renderUsElection2016Preview(await getUSElectionLocals());
 	})
 
 	// redirect from root
-	.redirect('/', '/us-election-2016/preview.html', 302)
+	.redirect('/', '/ai-robotics/preview.html', 302)
 ;
 
 // log in development
