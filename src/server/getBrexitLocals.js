f72557fa28a2bf6af3e052ebd0776f9b0e763f47
Martin Stabe <martinstabe@gmail.com>
2016-04-25 16:46:49 +0100

    Add countdown

--- src/server/getBrexitLocals.js
+++ src/server/getBrexitLocals.js
@@ -38,6 +38,8 @@ export async function fetchBerthaData() {
 		data[name] = value;
 	}
 
+	// date process: days 2016-06-23 roundded down to nearest full day
+	data.heading = data.heading + (new Date(new Date(2016, 05, 23) - new Date())).toUTCDays() + ' days until the referendum' 
 	// process text from markdown to html, then insert data-trackable attributes into any links
 	data.text = (() => {
 		const $ = cheerio.load(marked(data.text));
