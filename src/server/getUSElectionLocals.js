ba5ff2ed15d31872be885a2c0c790ebf81e1e5f6
Joanna Kao <joanna.kao@ft.com>
2016-07-16 14:13:43 -0400

    replace delegate counter with general election poll chart

--- src/server/getUSElectionLocals.js
+++ src/server/getUSElectionLocals.js
@@ -4,18 +4,11 @@ import Promise from 'bluebird';
 
 export default async function getUSElectionLocals() {
 	const contentURL = `http://bertha.ig.ft.com/view/publish/gss/${process.env.US_ELECTION_SPREADSHEET_KEY}/options,links`;
-	const resultsURL = `http://bertha.ig.ft.com/view/publish/gss/${process.env.US_ELECTION_RESULTS_SPREADSHEET_KEY}/results,options`;
 
-	const [contentRes, resultsRes] = await Promise.all([
-		Promise.resolve(fetch(contentURL))
-			.timeout(10000, new Error(`Timeout - bertha took too long to respond: ${contentURL}`)),
-
-		Promise.resolve(fetch(resultsURL))
-			.timeout(10000, new Error(`Timeout - bertha took too long to respond: ${resultsURL}`)),
-	]);
+	const contentRes = await Promise.resolve(fetch(contentURL))
+			.timeout(10000, new Error(`Timeout - bertha took too long to respond: ${contentURL}`));
 
 	if (!contentRes.ok) throw new Error(`Request failed with ${contentRes.status}: ${contentURL}`);
-	if (!resultsRes.ok) throw new Error(`Request failed with ${resultsRes.status}: ${resultsURL}`);
 
 	const data = {};
 
@@ -29,57 +22,22 @@ export default async function getUSElectionLocals() {
 		data[name] = value;
 	}
 
-	const allResultsSheets = await resultsRes.json();
-
-	// build a convenient options lookup
-	const resultsOptions = {};
-	for (const { name, value } of allResultsSheets.options) {
-		if (!name) {
-			throw new Error(
-				`Malformed content. Found an undefined option name in the spreadsheet: ${resultsURL}`
-			);
-		}
-		resultsOptions[name] = value;
+	// get poll chart SVG
+	async function fetchChart(width, height) {
+		const url = `https://ft-ig-us-elections-polltracker.herokuapp.com/polls.svg?fontless=true&startDate=June%207,%202016&size=${width}x${height}&type=area&state=us&logo=false`;
+		const res = await Promise.resolve(fetch(url))
+			.timeout(10000, new Error(`Timeout - us election poll took too long to respond: ${url}`));
+		if (!res.ok) throw new Error(`Request failed with ${res.status}: ${url}`);
+		return res.text();
 	}
 
-	// sort results by total delegates descending
-	let results = allResultsSheets.results;
-	results.sort((a, b) => b.total - a.total);
-
-	// only use the last name for each candidate
-	results = results.map(candidate => {
-		candidate.label = candidate.label.split(' ');
-		candidate.label = candidate.label[candidate.label.length - 1];
-
-		return candidate;
-	});
-
-	data.democratTotalToWin = resultsOptions.demdelegatestotal;
-	data.republicanTotalToWin = resultsOptions.repdelegatestotal;
-
-	// split democrats and republicans so we can get the top candidates from each party
-	const democrats = results.filter(candidate =>
-		!candidate.droppedout && candidate.party === 'democrats'
-	);
-	const republicans = results.filter(candidate =>
-		!candidate.droppedout && candidate.party === 'republicans'
-	);
-
-	// variables used inside template
-	data.democrats = democrats;
-	data.democrat100Percent = data.democratTotalToWin;
-
-	if (democrats[0].total > data.democratTotalToWin) {
-		data.democrat100Percent = democrats[0].total;
-	}
-
-	// variables used inside template
-	data.republicans = republicans;
-	data.republican100Percent = data.republicanTotalToWin;
-
-	if (republicans[0].total > data.republicanTotalToWin) {
-		data.republican100Percent = republicans[0].total;
-	}
+	data.pollCharts = await Promise.props({
+		default: fetchChart(300, 200),
+		S: fetchChart(400, 300),
+		M: fetchChart(400, 300),
+		L: fetchChart(300, 200),
+		XL: fetchChart(300, 250),
+	})
 
 	// process text from markdown to html, then insert data-trackable attributes into any links
 	data.text = (() => {
