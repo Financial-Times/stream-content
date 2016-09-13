699497b14bd5ca2535950a37dc10046f0e1c11b7
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-09-13 23:34:49 +0100

    Images as UUIDs or absolute Urls

--- src/server/getSummaryLocals.js
+++ src/server/getSummaryLocals.js
@@ -3,14 +3,40 @@ import Poller from './poller';
 const berthaUrl = 'http://bertha.ig.ft.com/view/publish/gss/1G2LIYU8eI4TmN8v-NYicknep6hMEdTv_QBl0cKSOGew/cards';
 const interval = '*/30 * * * * *';
 
+
 const poller = new Poller(interval, berthaUrl, function (data) {
+  const absUrl = /^https?\:\/\//;
+  const uuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+  function rowToCard(row) {
+
+    let renderResponsiveImage = false;
+    let image = null;
+
+    if (row.image) {
+      if (absUrl.test(row.image)) {
+        image = row.image;
+      } else {
+        image = `https://next-geebee.ft.com/image/v1/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2F${row.image}?source=ig_stream&fit=scale-down&compression=best&width=450`;
+        renderResponsiveImage = true;
+      }
+    }
+    return {
+      ...row,
+      id: row.id ? row.id.replace(/\s+/g, '').toLowerCase() : '',
+      image,
+      renderResponsiveImage,
+    };
+  }
+
   if (!Array.isArray(data) || !data.length) {
     throw new Error('Cannot get summary cards');
   }
 
   return data.reduce((map, row) => {
-    if (!row.id) return map;
-    return map.set(row.id, row);
+    const card = rowToCard(row);
+    if (!card.id) return map;
+    return map.set(card.id, card);
   }, new Map());
 });
 
