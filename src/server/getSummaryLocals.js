8924b8c26cd4ab08dc99851851d75d00cfed66d8
Luke Kavanagh <kavanagh@users.noreply.github.com>
2016-10-12 13:47:17 +0100

    Add extra card layouts (#77)
    * Add extra card layouts
    
    This is a first stab at providing other layouts

--- src/server/getSummaryLocals.js
+++ src/server/getSummaryLocals.js
@@ -1,15 +1,14 @@
 import Poller from './poller';
 
-const berthaUrl = 'http://bertha.ig.ft.com/view/publish/gss/1G2LIYU8eI4TmN8v-NYicknep6hMEdTv_QBl0cKSOGew/cards';
+const summaryCardSheetId = process.env.SUMMARY_CARD_SPREADSHEET || '1G2LIYU8eI4TmN8v-NYicknep6hMEdTv_QBl0cKSOGew';
+const berthaUrl = `http://bertha.ig.ft.com/view/publish/gss/${summaryCardSheetId}/cards`;
 const interval = '*/30 * * * * *';
-
 
 const poller = new Poller(interval, berthaUrl, function (data) {
   const absUrl = /^https?\:\/\//;
   const uuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
   function rowToCard(row) {
-
     let renderResponsiveImage = false;
     let image = null;
 
@@ -21,6 +20,7 @@ const poller = new Poller(interval, berthaUrl, function (data) {
         renderResponsiveImage = true;
       }
     }
+
     return {
       ...row,
       id: row.id ? row.id.replace(/\s+/g, '').toLowerCase() : '',
